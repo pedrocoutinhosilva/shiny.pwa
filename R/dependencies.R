a8dc435a0ad7d6f4f460d35f033ffb50db9edba3
pedrocoutinhosilva <pedrocoutinhosilva@gmail.com>
2020-05-08 20:51:40 +0200

    Extracted dependency management.

---
+++ R/dependencies.R
@@ -0,0 +1,52 @@
+#' Returns a set of tags required for registering the pwa.
+#'
+#' @param title Title of the app
+#' @param icon Name of the icon file to be user
+#' @param color color used for the UI on the installed pwa
+#' @param location subdirectory where the app is hosted. Only required if the app is not on the root domain.
+#'
+#' @return A UI definition that can be passed to the [shinyUI] function.
+#'
+#' @export
+loadDependencies <- function(title, icon, color, location = "") {
+  register_worker <- tags$script(
+    applyTemplate(
+      read_file(paste0(system.file("pwa", package = "shiny.pwa"), "/", "register-worker.js")),
+      list(location = location)
+    )
+  )
+
+  tags$head(
+    # Because of how scopes work for service workers
+    # the worker needs to be initialized directly from the index file.
+    register_worker,
+
+    tags$link(rel = "manifest", href = "pwa/manifest.json"),
+    tags$link(rel = "apple-touch-icon", href = icon),
+    tags$meta(name = "description", content = title),
+    tags$meta(name = "theme-color", content = color),
+    tags$meta(name = "apple-mobile-web-app-status-bar-style", content = color),
+    tags$meta(name = "apple-mobile-web-app-capable", content = "yes")
+  )
+}
+
+#' Generates the required files for enabling the pwa.
+#'
+#' @param title The title of your Shiny app
+#' @param domain the base URL where the app is hosted
+#' @param location subdirectory where the app is hosted. Only required if the app is not on the root domain.
+#' @param offline_template Path to the offline template you want to use. If left NULL the default template is used.
+#' @param offline_message When using the default offline page template, message to be displayed.
+#' @param icon Icon Path to be used for the app. Size should be 512x512px. If left NULL a default icon is provided.
+#' @param color Color of the app. Used to color the browser elements when the pwa is installed.
+#'
+#' @return A UI definition that can be passed to the [shinyUI] function.
+#'
+#' @export
+generateFiles <- function(title, domain, location, offline_template, offline_message, icon, color) {
+  createDirectories()
+  createServiceWorker()
+  createIcon(icon)
+  createOfflinePage(title, offline_template, offline_message)
+  createManifest(title, paste0(domain, location), color)
+}
--- R/supportPWA.R
+++ R/supportPWA.R
@@ -1,12 +1,12 @@
 #' Adds PWA app support.
 #'
-#' @param base_url the base URL where the app is hosted
+#' @param domain the base URL where the app is hosted
 #' @param title The title of your Shiny app
 #' @param location subdirectory where the app is hosted. Only required if the app is not on the root domain.
-#' @param icon Icon to be used for the app. Should be 512x512 and saved under `www/pwa/offline.html`. A default icon is provided.
-#' @param color Color for the app. Used when the app is installed to color the minified browser elements.
-#' @param use_offline_template Should the default offine template be used. Your own template can be created under `www/pwa/offline.html`
-#' @param offline_message When using the default offline page template, can be used to change the displayed message.
+#' @param icon Icon Path to be used for the app. Size should be 512x512px. If left NULL a default icon is provided.
+#' @param color Color of the app. Used to color the browser elements when the pwa is installed.
+#' @param offline_template Path to the offline template you want to use. If left NULL the default template is used.
+#' @param offline_message When using the default offline page template, message to be displayed.
 #'
 #' @return A UI definition that can be passed to the [shinyUI] function.
 #'
@@ -17,84 +17,17 @@
 #' supportPWA()
 #'
 #' @export
-supportPWA <- function(base_url,
+supportPWA <- function(domain,
                        title = "My Shiny PWA",
                        location = "",
                        icon = NULL,
                        color = "#000000",
-                       use_offline_template = TRUE,
-                       offline_message = "Looks like you are offline :O") {
-  package_dir <- system.file("pwa", package = "shiny.pwa")
-  project_dir <- getwd()
+                       offline_template = NULL,
+                       offline_message = "Looks like you are offline :/") {
+  domain   <- validateDomain(domain)
+  location <- validateLocation(location)
+  icon     <- validateIcon(icon)
 
-  # create /wwww folder if does not exist yet.
-  if (!dir.exists(paste0(project_dir, "/www"))) {
-    dir.create(paste0(project_dir, "/www"))
-  }
-
-  # create /wwww/pwa folder if does not exist yet
-  if (!dir.exists(paste0(project_dir, "/www/pwa"))) {
-    dir.create(paste0(project_dir, "/www/pwa"))
-  }
-
-  # if no icon is defined, use the default one.
-  if (is.null(icon)) {
-    icon <- "512.png"
-    file.copy(paste0(package_dir, "/", icon), paste0(project_dir, "/www/pwa"))
-  }
-
-  # if no offline HTML page is defined, use the default one.
-  if (use_offline_template) {
-    offline_page <- read_file(paste0(package_dir, "/", "offline.html"))
-
-    offline_arguments <- list(
-      offline_page,
-      title = title,
-      message = offline_message,
-      .open = "<<",
-      .close = ">>"
-    )
-    writeLines(
-      do.call(glue::glue, offline_arguments),
-      paste0(project_dir, "/www/pwa/offline.html")
-    )
-  }
-
-    # Creates the manifest file.
-  manifest_arguments <- list(
-    read_file(paste0(package_dir, "/", "manifest.json")),
-    name = title,
-    short_name = title,
-    start_url = glue::glue("{base_url}{location}"),
-    background_color = color,
-    theme_color = color,
-    description = title,
-    icon = icon,
-    .open = "<<",
-    .close = ">>"
-  )
-  writeLines(
-    do.call(glue::glue, manifest_arguments),
-    paste0(project_dir, "/www/pwa/manifest.json")
-  )
-
-  # Adds the service worker file.
-  file.copy(paste0(package_dir, "/", "service-worker.js"), paste0(project_dir, "/www/"))
-
-  # adds all tags regired to the header.
-  tags$head(
-    tags$link(rel = "manifest", href = "pwa/manifest.json"),
-    tags$link(rel = "apple-touch-icon", href = icon),
-    # Because of how scopes work for service workers, the worker needs to be initialized directly from the index file.
-    tags$script(HTML(glue::glue(
-      "if('serviceWorker' in navigator) {
-        navigator.serviceWorker
-                 .register('/<<location>>service-worker.js', { scope: '/<<location>>' })
-                 .then(function() { console.log('Service Worker Registered'); });
-      }", .open = "<<", .close = ">>"))),
-    tags$meta(name = "description", content = title),
-    tags$meta(name = "theme-color", content = color),
-    tags$meta(name = "apple-mobile-web-app-capable", content = "yes"),
-    tags$meta(name = "apple-mobile-web-app-status-bar-style", content = color)
-  )
+  generateFiles(title, domain, location, offline_template, offline_message, icon, color)
+  loadDependencies(icon, title, color, location)
 }
