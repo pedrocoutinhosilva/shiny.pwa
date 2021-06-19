c8fbcbe558cce12ea8e48f0ff7b21fa882d25b18
Pedro Silva <pedro@appsilon.com>
2021-06-19 16:27:13 +0200

    Add custom icon example.

---
+++ examples/pwa with icon/app.R
@@ -0,0 +1,61 @@
+library(shiny)
+library(shiny.pwa)
+
+ui <- bootstrapPage(
+
+  pwa(
+    "https://sparktuga.shinyapps.io/pwawithicon",
+    title = "PWA with Icon",
+    icon = "assets/icon.png",
+    output = "www",
+    color = "#654321"
+  ),
+
+  selectInput(inputId = "n_breaks",
+              label = "Number of bins in histogram (approximate):",
+              choices = c(10, 20, 35, 50),
+              selected = 20),
+
+  checkboxInput(inputId = "individual_obs",
+                label = strong("Show individual observations"),
+                value = FALSE),
+
+  checkboxInput(inputId = "density",
+                label = strong("Show density estimate"),
+                value = FALSE),
+
+  plotOutput(outputId = "main_plot", height = "300px"),
+
+  # Display this only if the density is shown
+  conditionalPanel(condition = "input.density == true",
+                   sliderInput(inputId = "bw_adjust",
+                               label = "Bandwidth adjustment:",
+                               min = 0.2, max = 2, value = 1, step = 0.2)
+  )
+
+)
+
+server <- function(input, output) {
+
+  output$main_plot <- renderPlot({
+
+    hist(faithful$eruptions,
+         probability = TRUE,
+         breaks = as.numeric(input$n_breaks),
+         xlab = "Duration (minutes)",
+         main = "Geyser eruption duration")
+
+    if (input$individual_obs) {
+      rug(faithful$eruptions)
+    }
+
+    if (input$density) {
+      dens <- density(faithful$eruptions,
+                      adjust = input$bw_adjust)
+      lines(dens, col = "blue")
+    }
+
+  })
+}
+
+shinyApp(ui, server)
